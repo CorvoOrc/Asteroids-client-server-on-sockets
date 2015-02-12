29f57508905ca7b9d20f501238959e1318384ffd
Alexander Steshenko <CorvoOrc@users.noreply.github.com>
2015-02-12 20:51:42 +0400

    Update Program.cs

--- Brain/Program.cs
+++ Brain/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,15 +10,32 @@ namespace Brain
     {
         static void Main(string[] args)
         {
-            /*if (args.Length < 1)
+            if (args.Length < 1)
             {
-                Console.WriteLine("Enter number of port (recommend from 10000 to 13000)");
+                Console.WriteLine("Don`t entered number of port (recommend from 10000 to 13000)");
                 return;
-            }*/
-            Int32 port = 13000; // Convert.ToInt32(args[0]);
+            }
+            
+            Int32 port = Convert.ToInt32(args[0]);
 
-            Brain brain = new Brain(port);
-            brain.Dream();
+            int minPort = 0;
+            int maxPort = 65535;
+            if (port < minPort || port > maxPort)
+            {
+                return;
+            }
+
+            Console.WriteLine("Entered port {0}", port);
+
+            try
+            {
+                Brain brain = new Brain(port);
+                brain.Dream();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+            }
         }
     }
 }
